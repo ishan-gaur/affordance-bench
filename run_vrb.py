c4d35d49a4f3df055dd94c0ec91997c6ef72eb98
Ishan Gaur <ishangaur4@gmail.com>
2023-12-06 23:04:01 +0100

    some nice functions for error display and progress bar

--- run_vrb.py
+++ run_vrb.py
@@ -1,15 +1,17 @@
 import sys
 from pathlib import Path
 import subprocess
-
+from tqdm import tqdm
+import subprocess
 vrb_repo = Path("~/vrb").expanduser()
 giga_repo = Path("~/GIGA").expanduser()
 
 def run_command_on_subdirectories(directory_path):
     directory = Path(directory_path)
     conda_env = "vrb"
+    rgb_images_list = []
 
-    for subdirectory in directory.iterdir():
+    for subdirectory in tqdm(directory.iterdir(), desc="Processing subdirectories"):
         if subdirectory.is_dir():
             rgb_images_folder = subdirectory / "rgb_images"
             vrb_folder = subdirectory / "vrb"
@@ -29,18 +31,33 @@ def run_command_on_subdirectories(directory_path):
             else:
                 vrb_folder.mkdir()
     
-            for file in rgb_images_folder.glob("*.png"):  # Iterate over .png files in rgb_images folder
-                rgb_image = file.name
-                
-                program = f"""
-                python {vrb_repo}/aff_bench_inference.py --image {subdirectory}/rgb_images/{rgb_image} \
-                --output {subdirectory}/rgb_images/{rgb_image} --obj_list {giga_repo}/object_list.txt \
-                --model_path {vrb_repo}/models/model_checkpoint_1249.pth.tar \
-                --max_box 5
-                """
-                # command = f"conda init bash && conda activate {conda_env} && {program}"
-                command = program
-                subprocess.run(command, cwd=subdirectory, shell=True, executable="/bin/bash")
+            for file in rgb_images_folder.glob("*.png"):
+                rgb_images_list.append(file)
+    
+    for rgb_image in tqdm(rgb_images_list, desc="Processing images"):
+        rgb_image_path = rgb_image.resolve()
+        subdirectory = rgb_image_path.parent.parent
+        
+        program = f"""
+        python {vrb_repo}/aff_bench_inference.py --image {rgb_image_path} \
+        --output {subdirectory}/vrb/{rgb_image.name} --obj_list {giga_repo}/object_list.txt \
+        --model_path {vrb_repo}/models/model_checkpoint_1249.pth.tar \
+        --max_box 5
+        """
+        command = program
+
+        def run_command(command):
+            process = subprocess.Popen(command, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout, _ = process.communicate()
+            stdout = stdout.decode("utf-8")
+            return stdout
+
+        stdout = run_command(command)
+        if "All done !" not in stdout:
+            print(f"Failed image: {rgb_image_path}")
+            print(f"Associated stdout: {stdout}")
+        else:
+            print(f"Processed image: {rgb_image_path}")
 
 # Example usage
 if len(sys.argv) != 2:
