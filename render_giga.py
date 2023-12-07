a098a0008eeddb0ea2d8b0ad3db2e1a757693235
Ishan Gaur <ishangaur4@gmail.com>
2023-12-06 22:51:39 -0800

    proper cropping+zoom for the scenes

--- render_giga.py
+++ render_giga.py
@@ -1,7 +1,9 @@
 import numpy as np
+from PIL import Image
 import trimesh
 from pathlib import Path
 import sys
+import math
 
 if len(sys.argv) != 2:
     print("Usage: python render_giga.py <path/to/experiment/directory/>")
@@ -16,16 +18,20 @@ for trial_folder in exp_folder.iterdir():
     if not image_folder.exists():
         image_folder.mkdir()
 
-    scene_size = None
+    sizes = []
     for grasp_path in grasps_folder.iterdir():
         if grasp_path.suffix != '.obj':
             continue
         grasp_mesh = trimesh.load_mesh(grasp_path)
+        sizes.extend(grasp_mesh.bounding_box.extents[:2])
+
+    scene_size = max(sizes)
+    for grasp_path in grasps_folder.iterdir():
+        if grasp_path.suffix != '.obj':
+            continu
+        grasp_mesh = trimesh.load_mesh(grasp_path)
         scene = trimesh.Scene()
         scene.add_geometry(grasp_mesh)
-
-        if scene_size == None:
-            scene_size = grasp_mesh.bounding_box.extents[0]
         scene.set_camera(distance=2.0 * scene_size, angles=[np.pi / 3, 0, 0])
         png = scene.save_image()
 
@@ -33,3 +39,9 @@ for trial_folder in exp_folder.iterdir():
         image_path = image_path.with_suffix('.png')
         with open(image_path, 'wb') as f:
             f.write(png)
+
+        img = Image.open(image_path)
+        width, height = img.size
+        t, b = 0, height
+        l, r = int(math.floor(width / 4)), int(math.ceil(width / 4 * 3))
+        img.crop((l, t, r, b)).save(image_path)
